0df2a5bad2b9209e6d9d1a1f0dffef0cd4d55f53
23navi <navisureka23@gmail.com>
2023-09-05 17:04:19 +0530

    eventlistener for tooltip

--- tooltip.js
+++ tooltip.js
@@ -3,11 +3,23 @@
 class ToolTip extends HTMLElement {
   constructor() {
     super();
+    this._toolTipContainer;
   }
   connectedCallback() {
     const toolTipIcon = document.createElement("span");
+    toolTipIcon.addEventListener("mouseenter", this._showToolTip);
+    toolTipIcon.addEventListener("mouseleave", this._hideToolTip);
     toolTipIcon.textContent = "(?)";
     this.appendChild(toolTipIcon);
+  }
+  _showToolTip() {
+    this._toolTipContainer = document.createElement("span");
+    this._toolTipContainer.textContent = "This is a tool tip text";
+    this.appendChild(this._toolTipContainer);
+  }
+
+  _hideToolTip() {
+    this.removeChild(this._toolTipContainer);
   }
 }
 
